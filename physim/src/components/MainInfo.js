140f9d348733c095dfcf25b02f5efc5984cdc02b
Fabian Castano <fabioc9675@gmail.com>
2023-10-26 11:44:22 -0500

    Implementation of logos to repository and youtube tutorials

--- physim/src/components/MainInfo.js
+++ physim/src/components/MainInfo.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaGithubSquare, FaYoutubeSquare } from "react-icons/fa";
 import "../styles/MainInfo.css";
 
 import description from "../textFiles/description.json";
@@ -10,7 +11,34 @@ export default function MainInfo() {
     <div className="MainInfo-pane">
       <div className="MainInfo-Title">{description.title}</div>
       <div className="MainInfo-Body">{description.text}</div>
-      <img className="MainInfo-Logo" src={logo} alt="" height="5%" />
+      <div className="MainInfo-Logo">
+        <a
+          className="grey-text text-lighten-4 right"
+          href="https://www.udea.edu.co"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img width="200px" src={logo} alt="" />
+        </a>
+      </div>
+      <div className="MainInfo-Repo">
+        <a
+          className="grey-text text-lighten-4"
+          href="https://github.com/fabioc9675/PhysicsSimulators.git"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaGithubSquare className="MainInfo-Icon" size="3em" />
+        </a>
+        <a
+          className="grey-text text-lighten-4"
+          href="https://www.youtube.com/watch?v=V2eaKFyslGQ&list=PLR3dpncZPGe8qDBX-vzmo5AVLnSAygJdQ&index=21&t=2s"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaYoutubeSquare className="MainInfo-Icon" size="3em" />
+        </a>
+      </div>
     </div>
   );
 }
